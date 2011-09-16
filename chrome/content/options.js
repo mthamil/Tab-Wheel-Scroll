cadd4b152f08c40e0653cb822cdc1a47cb28a0b8
Matthew Hamilton <matthamilton@live.com>
2011-09-16 16:18:48 -0500

    Added more files

---
+++ chrome/content/options.js
@@ -0,0 +1,23 @@
+
+function setDefaultSelections()
+{
+    var prefService = Components.classes["@mozilla.org/preferences-service;1"]
+        .getService(Components.interfaces.nsIPrefService);
+        
+    var tabScrollPrefs = prefService.getBranch("extensions.tabscroll.");
+    var downScrollsLeft = tabScrollPrefs.getBoolPref("downScrollsLeft");
+    if (downScrollsLeft)
+    {
+        var downIsLeftOption = document.getElementById("downIsLeft");
+        downIsLeftOption.setAttribute("selected", true);
+        var downIsRightOption = document.getElementById("downIsRight");
+        downIsRightOption.setAttribute("selected", false);
+    }
+    else
+    {
+        var downIsRightOption = document.getElementById("downIsRight");
+        downIsRightOption.setAttribute("selected", true);
+        var downIsLeftOption = document.getElementById("downIsLeft");
+        downIsLeftOption.setAttribute("selected", false);
+    }
+}
