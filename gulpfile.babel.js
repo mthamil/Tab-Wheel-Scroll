1e13c1c11cecc31559e5511f375813c1a66eeb19
Matthew Hamilton <matthamilton@live.com>
2016-06-27 23:32:11 -0500

    - Use babeljs for gulp file. - Upgrade to gulp 4. - Add npm script to call gulp.

---
+++ gulpfile.babel.js
@@ -0,0 +1,40 @@
+import gulp     from "gulp";
+import del      from "del";
+import jeditor  from "gulp-json-editor";
+import babel    from "gulp-babel";
+import moment   from "moment";
+import { exec } from "child_process";
+
+const config = {
+    in: "src/",
+    out: "dist/"
+};
+
+export let clean = done => 
+    del([`${config.out}*`], done);
+
+export let version = () =>
+    gulp.src(`${config.in}package.json`, { base: `${config.in}` })
+        .pipe(jeditor({
+            "version": `${moment.utc().format("YYYYMMDD.H")}.0`
+        }))
+        .pipe(gulp.dest(config.out));
+
+export let source = () =>
+    gulp.src([`${config.in}**/*.js`], { base: `${config.in}` })
+        .pipe(babel())
+        .pipe(gulp.dest(config.out));
+
+export let content = gulp.series(version, () =>
+    gulp.src([`${config.in}**`, `!${config.in}package.json`, `!${config.in}**/*.js`], { base: `${config.in}` })
+        .pipe(gulp.dest(config.out)));
+
+export let build = gulp.series(clean, source, content, done => {
+    exec(`${__dirname}/node_modules/.bin/jpm xpi`, { cwd: `${config.out}` }, (err, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        done(err);
+    });
+});
+
+export default build;
