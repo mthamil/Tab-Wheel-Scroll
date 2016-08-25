d27c3d87ec359a64f4b6e8a3b3155721a1c897ec
Matthew Hamilton <matthamilton@live.com>
2016-08-24 22:09:06 -0500

    Migrate preferences from old branch.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+import * as prefService   from "addon-sdk/lib/sdk/preferences/service";
+import { prefs } 	 	  from "addon-sdk/lib/sdk/simple-prefs";
 import { WindowManager }  from "./lib/WindowManager";
 import { TabWheelScroll } from "./lib/TabWheelScroll";
 
 let windowManager = null;
 
-export function main() {
+function migrate(preference) {
+	if (prefService.has(`extensions.extensions.tabscroll.${preference}`)) {
+		prefs[preference] = prefService.get(`extensions.extensions.tabscroll.${preference}`);
+	}
+}
+
+export function main(options) {
+	if (options.loadReason === "upgrade") {
+		[ "downScrollsLeft", "scrollWrap" ].forEach(p => migrate(p));
+	}
+
 	windowManager = new WindowManager(
 		(window) => new TabWheelScroll(window),
 		(tabWheelScroll) => tabWheelScroll.dispose());
