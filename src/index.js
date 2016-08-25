1ce95e5b3d8a88006b5873918cc25eaee4576daa
Matthew Hamilton <matthamilton@live.com>
2016-08-24 22:30:34 -0500

    Fix spaces vs tabs.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import * as prefService   from "addon-sdk/lib/sdk/preferences/service";
-import { prefs } 	 	  from "addon-sdk/lib/sdk/simple-prefs";
+import { prefs }          from "addon-sdk/lib/sdk/simple-prefs";
 import { WindowManager }  from "./lib/WindowManager";
 import { TabWheelScroll } from "./lib/TabWheelScroll";
 
 let windowManager = null;
 
 function migrate(preference) {
-	if (prefService.has(`extensions.extensions.tabscroll.${preference}`)) {
-		prefs[preference] = prefService.get(`extensions.extensions.tabscroll.${preference}`);
-	}
+    if (prefService.has(`extensions.extensions.tabscroll.${preference}`)) {
+        prefs[preference] = prefService.get(`extensions.extensions.tabscroll.${preference}`);
+    }
 }
 
 export function main(options) {
-	if (options.loadReason === "upgrade") {
-		[ "downScrollsLeft", "scrollWrap" ].forEach(p => migrate(p));
-	}
+    if (options.loadReason === "upgrade") {
+        [ "downScrollsLeft", "scrollWrap" ].forEach(p => migrate(p));
+    }
 
-	windowManager = new WindowManager(
-		(window) => new TabWheelScroll(window),
-		(tabWheelScroll) => tabWheelScroll.dispose());
+    windowManager = new WindowManager(
+        (window) => new TabWheelScroll(window),
+        (tabWheelScroll) => tabWheelScroll.dispose());
 }
 
 export function onUnload() {
-	windowManager.dispose();
+    windowManager.dispose();
 }
